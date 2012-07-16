65effa47b3cc0606fec75309589fb04bbe52f868
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-07-16 18:00:00 +0200

    Switch the way the cascading is done

--- template/unfold.ps1
+++ template/unfold.ps1
@@ -2,7 +2,10 @@ param(
     [Parameter(Position=0, Mandatory=0)]
     [string]$taskName,
     [Parameter(Position=1, Mandatory=0)]
-    [System.Collections.Hashtable]$properties = @{}
+    [System.Collections.Hashtable]$properties = @{},
+    [Parameter(Position=2, Mandatory=0)]
+    [switch]$docs = $false
+
 )
 
 $buildFile = "deploy.ps1"
@@ -12,15 +15,22 @@ $nologo = $true
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.path) 
 remove-module unfold -ErrorAction SilentlyContinue
 
-try {
-    #first load locally
-    import-module (join-path $scriptPath .\unfold\unfold.psm1) -ArgumentList $properties
-} catch {
-    # then from profile
-    import-module unfold -ArgumentList $properties
+# Prefer local import if exists
+$import = ".\unfold\unfold.psm1"
+
+If(-not (Test-Path $import)) {
+    $import = "unfold"
 }
 
-invoke-psake $buildFile $taskList "4.0" $null @{} $properties {} $nologo 
+# reload if already loaded
+If(Get-Module unfold) {
+    Remove-Module unfold
+}
+
+# then from profile
+import-module unfold -ArgumentList $properties
+
+invoke-psake $buildFile $taskList "4.0" $docs @{} $properties {} $nologo 
 
 Remove-Sessions
-remove-module unfold
+Remove-Module unfold
