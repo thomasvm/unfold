cccfcce98950e49162afc6fa7220282452f5c928
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-07-18 21:10:50 +0200

    Comment example

--- template/deploy.ps1
+++ template/deploy.ps1
@@ -6,14 +6,14 @@ task Default -depends "deploy"
 
 # Add custom tasks, for example one to
 # setup logs folder in root
-task createlogs -description "Creates logs folder in root" {
-    # Execute in remote location
-    Invoke-Script {
-        If(-not(Test-Path $config.basePath)) {
-            New-Item -type Directory -Name logs
-        }
-    }
-}
-
-# Set it to execute after setup
-Set-AfterTask setup createlogs
+# task createlogs -description "Creates logs folder in root" {
+#     # Execute in remote location
+#     Invoke-Script {
+#         If(-not(Test-Path $config.basePath)) {
+#             New-Item -type Directory -Name logs
+#         }
+#     }
+# }
+# 
+# # Set it to execute after setup
+# Set-AfterTask setup createlogs
