0acea2fc30de9d48a785691b0ffd0d6e74e36b48
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-07-20 23:14:03 +0200

    Add nuget packaging of unfold for easy installation

---
+++ package/default.ps1
@@ -0,0 +1,41 @@
+$config = New-Object PSObject -property @{ 
+    version="0.0.1";
+    releaseNotes=@"
+Initial release
+"@
+}
+
+task Cleanup {
+    Remove-Item -Recurse pkg -ErrorAction silentlycontinue
+}
+
+task Setup -depends Cleanup {
+    New-Item -type Directory pkg
+
+    $contentFolder = "pkg\content\deployment\unfold"
+
+    New-Item -type Directory $contentFolder
+    Copy-Item ..\tasks.ps1 $contentFolder
+    Copy-Item ..\unfold.psm1 $contentFolder
+    Copy-Item -Recurse ..\lib $contentFolder
+
+    Copy-Item ..\template\* "pkg\content\deployment"
+}
+
+task Package -depends Setup {
+    Copy-Item unfold.nuspec pkg
+    cd pkg
+    SetNuSpecVersionInfo
+    ..\.nuget\NuGet.exe pack
+    cd ..
+    Copy-Item pkg\Unfold.*.nupkg .
+    Remove-Item -Recurse pkg
+}
+
+function SetNuSpecVersionInfo {
+    (Get-Content "Unfold.nuspec") | Foreach-Object {
+        $_ -replace 'PKG_VERSION', $config.version `
+           -replace 'PKG_RELEASENOTES', $config.releaseNotes
+        } | Set-Content "pkg\Cloudinary.nuspec"
+}
+
