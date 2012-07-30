a0414de6bb6a7b535acff46411e9a518443d6200
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-07-30 21:00:27 +0200

    Detect remote location when installing through nuget

--- package/Install.ps1
+++ package/Install.ps1
@@ -4,8 +4,52 @@ $name = $project.Name
 $projectPath = Split-Path $project.Filename
 $deployFilePath = "$projectPath\deployment\deploy.ps1"
 
+function Detect-RemoteOrigin($folder) {
+    while($folder) {
+        Write-Host $folder
+        If(Test-Path "$folder\.git") {
+            # read config
+            $config = Get-Content "$folder\.git\config" -ErrorAction SilentlyContinue
+    
+            $inOriginSection = $false
+    
+            # detect remote origin section, and read url
+            foreach($line in $config) {
+                If ($inOriginSection) {
+                  $parts = $line.Split('=') | ForEach-Object { $_.Trim() }
+    
+                  If($parts[0] -eq "url") {
+                    return $parts[1]
+                  }
+    
+                } ElseIf($line -eq '[remote "origin"]') {
+                    $inOriginSection = $true
+                } ElseIf ($line.StartsWith("[")) {
+                    $inOriginSection = $false
+                }
+            }
+    
+            break
+        }
+    
+        # Top-level? break
+        If(-not(Split-Path $folder).Length) {
+            break
+        }
+        $folder = Get-Item "$folder\.."
+    }
+}
+
+
+$remoteOrigin = Detect-RemoteOrigin $projectPath
+
+If(-not $remoteOrigin) {
+    $remoteOrigin = "<gitrepository>"
+}
+
 (Get-Content $deployFilePath) | Foreach-Object {
     $_ -replace '<projectname>', $name `
-       -replace '<deploypath>', "c:\inetpub\wwwroot\$name"
+       -replace '<deploypath>', "c:\inetpub\wwwroot\$name" `
+       -replace '<gitrepository>', $remoteOrigin
     } | Set-Content $deployFilePath
 
