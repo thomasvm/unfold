d4ab6240a42a8f0d9695e7ace64e953080220f93
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-08-04 16:59:31 +0200

    Add examples launcher

---
+++ examples/unfold.ps1
@@ -0,0 +1,33 @@
+param(
+    [Parameter(Position=0, Mandatory=1)]
+    [string]$buildFile,
+    [Parameter(Position=1, Mandatory=0)]
+    [string]$taskName,
+    [Parameter(Position=2, Mandatory=0)]
+    [System.Collections.Hashtable]$properties = @{},
+    [Parameter(Position=3, Mandatory=0)]
+    [switch]$docs = $false
+)
+
+$taskList = @($taskName)
+$nologo = $true
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.path) 
+remove-module unfold -ErrorAction SilentlyContinue
+
+# First try local unfold
+$import = join-path $scriptPath .\unfold\unfold.psm1
+
+# Then from profile
+If(-not (Test-Path $import)) {
+    $import = "unfold"
+}
+
+import-module $import -ArgumentList $properties
+
+invoke-psake $buildFile $taskList "4.0" $docs @{} $properties {
+    Initialize-Configuration
+} $nologo 
+
+Remove-Sessions
+remove-module unfold
