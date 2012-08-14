d670b3c70371d19321399655f35313284dbf32a6
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-08-14 22:05:53 +0200

    build file is fixed for nuget packaging

--- package/psake.ps1
+++ package/psake.ps1
@@ -6,27 +6,27 @@
 # otherwise named parameter binding fails
 param(
     [Parameter(Position=0,Mandatory=0)]
-    [string]$buildFile = 'default.ps1',
-    [Parameter(Position=1,Mandatory=0)]
     [string[]]$taskList = @(),
-    [Parameter(Position=2,Mandatory=0)]
+    [Parameter(Position=1,Mandatory=0)]
     [string]$framework,
-    [Parameter(Position=3,Mandatory=0)]
+    [Parameter(Position=2,Mandatory=0)]
     [switch]$docs = $false,
-    [Parameter(Position=4,Mandatory=0)]
+    [Parameter(Position=3,Mandatory=0)]
     [System.Collections.Hashtable]$parameters = @{},
-    [Parameter(Position=5, Mandatory=0)]
+    [Parameter(Position=4, Mandatory=0)]
     [System.Collections.Hashtable]$properties = @{},
-    [Parameter(Position=6, Mandatory=0)]
+    [Parameter(Position=5, Mandatory=0)]
     [alias("init")]
     [scriptblock]$initialization = {},
-    [Parameter(Position=7, Mandatory=0)]
+    [Parameter(Position=6, Mandatory=0)]
     [switch]$nologo = $false,
-    [Parameter(Position=8, Mandatory=0)]
+    [Parameter(Position=7, Mandatory=0)]
     [switch]$help = $false,
-    [Parameter(Position=9, Mandatory=0)]
+    [Parameter(Position=8, Mandatory=0)]
     [string]$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.path)
 )
+
+$buildFile = 'default.ps1'
 
 # '[p]sake' is the same as 'psake' but $Error is not polluted
 remove-module [p]sake
