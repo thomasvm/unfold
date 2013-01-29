7199c2a5c9ec358f834702d629f614bc154fd58f
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2013-01-29 20:36:21 +0100

    document dbup migrations

--- lib/migrations/dbup.ps1
+++ lib/migrations/dbup.ps1
@@ -1,3 +1,22 @@
+# Support for DbUp based migrations. 
+#
+# DbUp migrations typically are put in a separate project inside your 
+# solution. This project creates a self-contained executable that
+# is able to migrate your database. 
+#
+# The deployment extension consists of three step
+# * building the project in order to generate the migrations assembly
+# * putting the build result inside a database folder under an unfold release
+# * executing the migration
+#
+# Configuration options
+# Set-Config migrations dbup # tell unfold to include this file
+# Set-Config dbup @{
+#   msbuild = ".\code\path\to\dbupproject.csproj" # path to dbup project
+#   extraargs = "--extra args --for theassembly"
+# }
+# Set-Config automigrate $true # this will automatically run migrations on deployment
+
 task buildmigrations {
     If(-not $config.dbup.msbuild) {
         Write-Warning "No migrations msbuild project configured"
